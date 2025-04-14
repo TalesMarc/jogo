fa836767a338168e7a7aa712ef153d792eddf220
TalesMarc <halflostsoul0@gmail.com>
2025-04-14 20:01:11 -0300

    Add files via upload

---
+++ files 19.53.01/config.py
@@ -0,0 +1,22 @@
+# Configurações globais e constantes
+
+# Dimensões iniciais da tela
+initial_width = 800
+initial_height = 600
+
+# Cores (RGB)
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 128, 0)
+dark_green = (0, 100, 0)
+red = (255, 0, 0)
+grey = (50, 50, 50)
+light_grey = (100, 100, 100)
+light_blue = (173, 216, 230)  # Azul claro para o fundo
+
+# Estados do menu
+MENU_PRINCIPAL = 0
+MENU_JOGAR = 1
+MENU_CONFIGURACOES = 2
+MENU_SOLO = 3
+MENU_LANGUAGE = 4  # Adicionado para o menu de idiomas
